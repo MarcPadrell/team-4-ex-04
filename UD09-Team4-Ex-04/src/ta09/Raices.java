4ecc7519f54e03c0dd8c68eaa0b7b209995b9db6
MarcPadrell <marc.padrell@hotmail.com>
2022-04-14 09:52:45 +0200

    Constructor done!

--- UD09-Team4-Ex-04/src/ta09/Raices.java
+++ UD09-Team4-Ex-04/src/ta09/Raices.java
@@ -2,4 +2,18 @@ package ta09;
 
 public class Raices {
 
+	private double a;
+	private double b;
+	private double c;
+
+	public Raices() {
+		this(0.0,0.0,0.0);
+	}
+
+	public Raices(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
 }
